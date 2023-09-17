f3d7c81d38ec5ed8bbfa19b380454a0e32860afe
han-mg <alrb1277@gmail.com>
2023-09-17 23:40:20 +0900

    합치기

--- Glover_main/urls.py
+++ Glover_main/urls.py
@@ -6,6 +6,6 @@ from .views import *
 
 urlpatterns = [
 	path('', main, name='main'),
- 	path('user_page/', user_page, name='user_page'),
+    path('user_page/', user_page, name='user_page'),
     path('manager_page/', manager_page, name='manager_page'),
 ]
